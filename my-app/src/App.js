9d9ff6336a3d0c2c8faa455bca090a29e1d00cce
abdallah <abddoullah.bouzhar@gmail.com>
2023-01-05 23:18:02 +0100

    boredAPI

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,11 +2,7 @@ import React from 'react';
 
 function App() {
 
-    return (
-        <div className='bg-red-400'>
-            <h1>hello react</h1>
-        </div>
-    );
+    return 
 }
 
 export default App;
